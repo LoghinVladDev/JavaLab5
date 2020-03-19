402d53731d95107369c447bb355782860eb3e7d1
G2Gmycatisonfire <49059319+G2Gmycatisonfire@users.noreply.github.com>
2020-03-19 22:49:50 +0200

    Third Commit Refactor as Maven

--- src/main/java/ro/uaic/info/utility/LoadCommand.java
+++ src/main/java/ro/uaic/info/utility/LoadCommand.java
@@ -1,7 +1,33 @@
 package ro.uaic.info.utility;
 
 public class LoadCommand implements Command {
+    private String path;
 
+    public static class Builder{
+        private String path;
+
+        public enum type{
+            PLAIN_TEXT,
+            SERIALIZED
+        }
+
+        public Builder withPath(String path){
+            this.path = path;
+            return this;
+        }
+
+        public LoadCommand build(){
+            LoadCommand cmd = new LoadCommand();
+
+            cmd.path = this.path;
+
+            return cmd;
+        }
+    }
+
+    private LoadCommand(){
+
+    }
 
     public Object execute(){
         return null;
